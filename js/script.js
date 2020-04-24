9062cecf43f8ed3a5853f5a719ed95fcd8bceeb5
Alex <sasha-a@ukr.net>
2020-04-24 23:37:04 +0300

    Add html + css

---
+++ js/script.js
@@ -0,0 +1,15 @@
+window.addEventListener('DOMContentLoaded', function() {
+
+    let display = document.querySelector('.display'),
+        keys = document.querySelector('.keys'),
+        add = document.querySelector('#add'),
+        subtract = document.querySelector('#subtract'),
+        multiply = document.querySelector('#multiply'),
+        divide = document.querySelector('#divide'),
+        button = document.querySelectorAll('button'),
+        decimal = document.querySelector('#decimal'),
+        clear = document.querySelector('#clear'),
+        equal = document.querySelector('#equal');
+
+    
+});
