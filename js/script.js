fcd50b5e5b0db73f8c67121261d4af0db26c7a02
Alex <sasha-a@ukr.net>
2020-04-27 00:05:46 +0300

    add happy pass, but yet still one bug

--- js/script.js
+++ js/script.js
@@ -1,62 +1,80 @@
 window.addEventListener('DOMContentLoaded', function() {
 
-    let calculator = document.querySelector('.calculator'),
-        keys = calculator.querySelector('.keys'),
-        display = document.querySelector('.display');
+    'use strict';
 
-    keys.addEventListener('click', function(event) {
-        if (event.target.matches('button')) {
+    let calculator = document.querySelector('.calculator'),
+        keys = document.querySelector('.calculator__keys'),
+        display = document.querySelector('.calculator__display');
+
+    keys.addEventListener('click', e => {
+        if (e.target.matches('button')) {
                     
-            let key = event.target,
+            let key = e.target,
                 action = key.dataset.action,
                 keyContent = key.textContent,
                 displayedNum = display.textContent,
-                previuosKeyType = calculator.dataset.previousKeyType;
+                previousKeyType = calculator.dataset.previousKeyType;
             
-                if (!action) {
-                    console.log('Number key');
-                    if (displayedNum === '0' || previuosKeyType === 'operator') {
-                        display.textContent = keyContent;
-                    } else {
-                        display.textContent = displayedNum + keyContent;
-                        
-                    }                    
-                }
+            if (!action) {
+                console.log('Number key');
+                if (displayedNum === '0' || previousKeyType === 'operator') {
+                    display.textContent = keyContent;
+                } else {
+                    display.textContent = displayedNum + keyContent;
+                }                    
+            }
 
             // remove darkKeyMode from all keys
             Array.from(key.parentNode.children)
                 .forEach(k => k.classList.remove('darkKeyMode'));
 
-                if (action === 'add' || action === 'subtract' || action === 'multiply' || action === 'divide') {
-                    console.log('Operator\'s key');
+            if (action === 'add' || 
+                action === 'subtract' || 
+                action === 'multiply' || 
+                action === 'divide'
+               ){
+                    console.log('Operator key');
                     key.classList.add('darkKeyMode');
                     // add custom attribute
                     calculator.dataset.previousKeyType = 'operator';
+                    calculator.dataset.firstValue = displayedNum;
+                    calculator.dataset.operator = action;
+            }
+
+            if (action === 'decimal') {
+                console.log('Decimal key');
+                display.textContent = displayedNum + '.';
+            }
+
+            if (action === 'clear') {
+                console.log('Clear key');
+                display.textContent = '0';
+            }
+
+            let calculate = (n1, operator, n2) => {
+                let result = '';
+                if (operator === 'add') {
+                    result = parseFloat(n1) + parseFloat(n2);
+                } else if (operator === 'subtract') {
+                    result = parseFloat(n1) - parseFloat(n2);
+                } else if (operator === 'multiply') {
+                    result = parseFloat(n1) * parseFloat(n2);
+                } else if (operator === 'devide') {
+                    result = parseFloat(n1) / parseFloat(n2);
                 }
+                return result;
+            };
 
-                if (action === 'decimal') {
-                    console.log('Decimal key');
-                    display.textContent = displayedNum + '.';
-                }
+            if (action === 'calculate') {
+                console.log('Calculate key');
+                let firstValue = calculator.dataset.firstValue,
+                    operator = calculator.dataset.operator,
+                    secondValue = displayedNum;
 
-                if (action === 'clear') {
-                    console.log('Clear key');
-                    display.textContent = '0';
-                }
+                display.textContent = calculate(firstValue, operator, secondValue);
+            }
 
-                if (action === 'calculate') {
-                    console.log('Calculate key');
-                    let secondValue = displayedNum;
-                }
-
-        }
-
-
-        // Закончил на - To get the first number
-        
-
+            
+        }        
     });
-
-    
-    
 });
