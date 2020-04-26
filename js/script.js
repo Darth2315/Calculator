b50e61012e764047da4f9e65cc41893d3f757dc3
Alex <sasha-a@ukr.net>
2020-04-26 15:09:32 +0300

    add interactive for number and operator's keys. Next step - calculation

--- js/script.js
+++ js/script.js
@@ -1,15 +1,62 @@
 window.addEventListener('DOMContentLoaded', function() {
 
-    let display = document.querySelector('.display'),
-        keys = document.querySelector('.keys'),
-        add = document.querySelector('#add'),
-        subtract = document.querySelector('#subtract'),
-        multiply = document.querySelector('#multiply'),
-        divide = document.querySelector('#divide'),
-        button = document.querySelectorAll('button'),
-        decimal = document.querySelector('#decimal'),
-        clear = document.querySelector('#clear'),
-        equal = document.querySelector('#equal');
+    let calculator = document.querySelector('.calculator'),
+        keys = calculator.querySelector('.keys'),
+        display = document.querySelector('.display');
 
+    keys.addEventListener('click', function(event) {
+        if (event.target.matches('button')) {
+                    
+            let key = event.target,
+                action = key.dataset.action,
+                keyContent = key.textContent,
+                displayedNum = display.textContent,
+                previuosKeyType = calculator.dataset.previousKeyType;
+            
+                if (!action) {
+                    console.log('Number key');
+                    if (displayedNum === '0' || previuosKeyType === 'operator') {
+                        display.textContent = keyContent;
+                    } else {
+                        display.textContent = displayedNum + keyContent;
+                        
+                    }                    
+                }
+
+            // remove darkKeyMode from all keys
+            Array.from(key.parentNode.children)
+                .forEach(k => k.classList.remove('darkKeyMode'));
+
+                if (action === 'add' || action === 'subtract' || action === 'multiply' || action === 'divide') {
+                    console.log('Operator\'s key');
+                    key.classList.add('darkKeyMode');
+                    // add custom attribute
+                    calculator.dataset.previousKeyType = 'operator';
+                }
+
+                if (action === 'decimal') {
+                    console.log('Decimal key');
+                    display.textContent = displayedNum + '.';
+                }
+
+                if (action === 'clear') {
+                    console.log('Clear key');
+                    display.textContent = '0';
+                }
+
+                if (action === 'calculate') {
+                    console.log('Calculate key');
+                    let secondValue = displayedNum;
+                }
+
+        }
+
+
+        // Закончил на - To get the first number
+        
+
+    });
+
+    
     
 });
